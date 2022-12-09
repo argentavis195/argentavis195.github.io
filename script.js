b9aebdcd136420ac6c8a79fd63d186246678f717
Dmitry <69970526+argentavis195@users.noreply.github.com>
2022-12-09 20:31:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 let color = true;
 let main_block = document.querySelector('.main-block')
+
+const wind_text = document.querySelector(".wind_text");
+const second_celsium = document.querySelector(".second_celsium");
+const temp = document.querySelector(".temp");
+const sign_celsium = document.querySelector(".sign_celsium");
+const temp_feelLike = document.querySelector(".temp_feelLike");
+const wind  = document.querySelector(".wind")
+const temp_text = document.querySelector(".temp_text")
+const temp_feelLike_text = document.querySelector(".temp_feelLike_text")
+let orange_elements = [wind_text, second_celsium, temp, sign_celsium, temp_feelLike, wind, temp_text, temp_feelLike_text];
+
+function changeToOrange() {
+    for (let i = 0; i < orange_elements.length; i++) {
+        orange_elements[i].style.color = 'orange'
+    }
+}
+
+function changeToBlack() {
+    for (let i = 0; i < orange_elements.length; i++) {
+        orange_elements[i].style.color = 'black'
+    }
+}
+
+
 document.querySelector('.changeColorButton').addEventListener('click', function changeColorButton(params) {
     color = !color
-    if(color == true){
-        document.body.style.background = '#28282B'
-        document.querySelector('.changeColorButton').style.background = 'orange'
-        main_block.style.border = '1px solid orange'
-        document.querySelector(".temp").style.color = 'orange'
-        document.querySelector(".sign_celsium").style.color = 'orange'
-        document.querySelector(".temp_feelLike").style.color = 'orange'
-        document.querySelector(".wind").style.color = 'orange'
-        document.querySelector(".temp_text").style.color = 'orange'
-        document.querySelector(".temp_feelLike_text").style.color = 'orange'
-        document.querySelector(".wind_text").style.color = 'orange'
-        document.querySelector(".second_celsium").style.color = 'orange'
+    if(color){
+    document.body.style.background = '#28282B'
+    document.querySelector('.changeColorButton').style.background = 'orange'
+    main_block.style.border = '1px solid orange'
+
+        changeToOrange()
     }
     else{
         document.body.style.background = '#EDEADE'
         document.querySelector('.changeColorButton').style.background = '#36454F'
         main_block.style.border = '1px solid black'
         document.querySelector(".temp").style.color = 'black'
-        document.querySelector(".sign_celsium").style.color = 'black'
-        document.querySelector(".temp_feelLike").style.color = 'black'
-        document.querySelector(".wind").style.color = 'black'
-        document.querySelector(".temp_text").style.color = 'black'
-        document.querySelector(".temp_feelLike_text").style.color = 'black'
-        document.querySelector(".wind_text").style.color = 'black'
-        document.querySelector(".second_celsium").style.color = 'black'
+
+        changeToBlack()
     }
 })
 
@@ -34,7 +47,6 @@ document.querySelector('.changeColorButton').addEventListener('click', function 
 document.querySelector('.input').addEventListener("keypress", function(event) {
 let input = document.querySelector('.input').value
   if (event.key === "Enter") {
-    let signs = document.querySelectorAll('.sign_celsium')
 //for(let i = 0; i < signs.length; i++){
   //signs[i].style.display =  'inline-block'
 //}
@@ -47,16 +59,16 @@ let input = document.querySelector('.input').value
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=5488933573bee20246e8dc2177747d11`)
     .then((response) => response.json())
         .then(function(resp) {
-          let temp_resp = resp.main.temp;
-          let temp = document.querySelector('.temp').textContent =  Math.floor(temp_resp - 273.15)
+          const temp_resp = resp.main.temp;
+          document.querySelector('.temp').textContent =  Math.floor(temp_resp - 273.15)
 
 
-          let temp_feelLike_resp = resp.main.feels_like
-          let temp_feelLike = document.querySelector('.temp_feelLike').textContent = Math.floor(temp_feelLike_resp - 273.15)
+          const temp_feelLike_resp = resp.main.feels_like
+          const temp_feelLike = document.querySelector('.temp_feelLike').textContent = Math.floor(temp_feelLike_resp - 273.15)
 
 
           let wind_resp = resp.wind.speed
-          let wind = document.querySelector('.wind').textContent = wind_resp
+          document.querySelector('.wind').textContent = wind_resp
           })
     .catch(err => alert(`Погоды по этому пункту (${input}) к сожалению, на сайте нет.`))
   }
